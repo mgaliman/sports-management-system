1b0201546a098415ddad9c636f6e7689e21b2d2b
Marko Galiman <61890185+mgaliman@users.noreply.github.com>
2025-05-10 20:14:01 +0200

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // enable class validator globally
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Sports Class Management API')
+    .setDescription('API docs for the Sports complex management system')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT support
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
